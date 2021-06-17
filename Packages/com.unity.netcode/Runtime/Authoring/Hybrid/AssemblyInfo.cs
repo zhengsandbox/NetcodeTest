ad1225048ee2203da1f538c37558ba019f972738
leon.qu <zhengqu53@hotmail.com>
2021-06-17 18:26:20 +0100

    remove custom NETCODE

--- Packages/com.unity.netcode/Runtime/Authoring/Hybrid/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.NetCode.EditorTests")]
-[assembly: InternalsVisibleTo("Unity.NetCode.TestsUtils")]
-[assembly: InternalsVisibleTo("Unity.NetCode.Editor")]
-
